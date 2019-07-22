52bbabaa6ea3cf2df974bbc096dc1b4b92b15954
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-07-22 15:20:21 +0300

    oath initial staff

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+//const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+//mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'});
-})
+
+
+
+require('./services/passport');
+
+//require('./models/User')
+
+
+require('./routs/authRouts')(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => console.log('yes, mam'));
+
+
---
+++ services/passport.js
@@ -0,0 +1,13 @@
+const keys = require('../config/keys')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+}));
+
