73370d0dae4807a3f442e41b080cc3d332e53263
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-07-24 13:47:51 +0300

    auth done

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const coockieSession = require('cookie-session');
+const passport = require('passport');
 
-//mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const app = express();
 
+app.use(coockieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+.catch(err => console.log(err));
+mongoose.connection.on('connected', () => console.log('connected to database'));
+mongoose.connection.on('disconnected', () => console.log('OMG not connected'));
+
+require('./models/User');
 
 require('./services/passport');
 
-//require('./models/User')
+
 
 
 require('./routs/authRouts')(app);
